91dbda37fb5931f9bcd0afcc20571ee232f8c4d2
mariagiuliaruga <ruga.2051428@studenti.uniroma1.it>
2025-05-06 13:53:47 +0200

    init

--- init.sql
+++ init.sql
@@ -12,3 +12,11 @@ CREATE TABLE IF NOT EXISTS stili_donna (
     immagine VARCHAR(255) NOT NULL PRIMARY KEY,
     testo VARCHAR(255) NOT NULL
 );
+
+INSERT INTO utenti (email, password, data_registrazione) VALUES ('stefi@star', 's', '2025-05-06 13:41:00');
+
+
+DROP USER IF EXISTS 'admin'@'localhost';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON mystyleessence_db.* TO 'admin'@'localhost' IDENTIFIED BY 'pass';
+FLUSH PRIVILEGES;
