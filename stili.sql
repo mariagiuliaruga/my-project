786b9fda9410ea45771a0b87fff44a139444b6d0
stefaniaperera <perera.2050510@studenti.uniroma1.it>
2025-05-06 13:06:25 +0200

    stili donna bitch

--- stili.sql
+++ stili.sql
@@ -1,11 +1,14 @@
-CREATE DATABASE mystileessence_db IF NOT EXISTS;
-USE mystileessence_db;
+CREATE DATABASE IF NOT EXISTS mystyleessence_db;
+USE mystyleessence_db;
 
-CREATE TABLE utenti (
+CREATE TABLE IF NOT EXISTS utenti (
     email VARCHAR(255) NOT NULL PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     data_registrazione DATETIME
 );
 
 
-
+CREATE TABLE IF NOT EXISTS stili_donna (
+    immagine VARCHAR(255) NOT NULL PRIMARY KEY,
+    testo VARCHAR(255) NOT NULL
+);
