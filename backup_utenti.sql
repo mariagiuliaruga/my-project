22b413befa258a641b6b1fdd38afc2083b860237
mariagiuliaruga <ruga.2051428@studenti.uniroma1.it>
2025-05-07 13:44:53 +0200

    sempre que

--- backup_utenti.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO utenti (email, password, data_registrazione) VALUES ('prova@a', '$2y$10$W7Mwsq7xZZ4nLBDDBEDF0u4P14oB8hwmmCwZSobZROgAC58gSYd4m', '2025-05-06 22:43:20');
-INSERT INTO utenti (email, password, data_registrazione) VALUES ('nuovotest@test', '$2y$10$suoVfxEn.00WbRx.LRFa2uOJq2pkx9U5xfterrshnWu4GK9geEa4O', '2025-05-07 11:49:06');
-INSERT INTO utenti (email, password, data_registrazione) VALUES ('stefaniaperera6@gmail.com', '$2y$10$gD06OIcms4xmXiYyVPeOTOiUcCyw0rydp9N0H3IpFTWGcAZc3gLDK', '2025-05-07 11:52:10');
-INSERT INTO utenti (email, password, data_registrazione) VALUES ('mari@m', '$2y$10$IRAx/Bz/vDoeJxE60g9D/OwsEBfHHxFbyb7wh5vzOvaB.s0zdrS.K', '2025-05-07 11:57:03');
-INSERT INTO utenti (email, password, data_registrazione) VALUES ('mariagiuliaruga@gmail.com', '$2y$10$wvvGAyoTuI.8dcS/PzSQ5uxgaSocmA1VlX51H8AmbanCqqbwpGV4i', '2025-05-07 13:04:53');
