4447130081d9967d98af3805a23c52e32e0bc383
mariagiuliaruga <ruga.2051428@studenti.uniroma1.it>
2025-05-15 15:09:42 +0200

    camerino funzionante

--- caricamentoHome.js
+++ caricamentoHome.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.scrollTo(0, 0);    
 });
+
+document.querySelector('.explore-link').addEventListener('click', function(e){
+        e.preventDefault();
+
+        const targetId = this.getAttribute('data-target');
+        const targetContainer = document.getElementById(targetId);
+
+        if(targetContainer){
+            targetContainer.classList.add('visible');
+        }
+
+        window.scrollTo({ top: 0, behavior: 'smooth'});
+});
+
+// funziona solo se scritto in questo punto. serve per far caricare l'area stili dall'alto
