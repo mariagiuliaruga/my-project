5f1bf74ab0b6df448c6b54e9bb52238b36611b48
mariagiuliaruga <ruga.2051428@studenti.uniroma1.it>
2025-05-21 18:16:58 +0200

    ora funziona register

--- php/backup_utenti.sql
+++ php/backup_utenti.sql
@@ -1,3 +1,5 @@
 INSERT INTO utenti (email, password, data_registrazione) VALUES ('antonellascicchi@yahoo.it', '$2y$10$51R4egKMfeCKPPtBULxj9OVU3M0QxPS11qzAjHBHx8bspnPw3udsi', '2025-05-21 17:21:29');
 INSERT INTO utenti (email, password, data_registrazione) VALUES ('ruga@a', '$2y$10$Cq1FLLA4SvSsmSH5VGSlHuC0pdpqY.1FaMDNn2K0pyQaOxRtv0gMu', '2025-05-21 17:32:46');
 INSERT INTO utenti (email, password, data_registrazione) VALUES ('s@sss', '$2y$10$z4/2XOc3MmmQQiVwlDlIye.jJSGjOYlLm8R7StNy6/Yc61jSsFXOS', '2025-05-21 17:34:43');
+INSERT INTO utenti (email, password, data_registrazione) VALUES ('ciao@a', '$2y$10$jPm08QmdxZN3CgQjmYv5MOyoOTgHS5LEIuU4BgM8DcoJbUEEJjN0W', '2025-05-21 17:59:51');
+INSERT INTO utenti (email, password, data_registrazione) VALUES ('marco@marco', '$2y$10$R.h49/KXe.j84yjHZwirW.aKBprps3YAquGsvlFy7L9qB6O3kVN9K', '2025-05-21 18:15:53');
